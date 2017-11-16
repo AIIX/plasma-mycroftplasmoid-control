ef1e69a6b73d52a1ccc2843833a9ab0a24b1f9e3
Aditya Mehra <Aix.m@outlook.com>
2017-11-16 18:54:22 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# plasma-mycroftplasmoid-control
-A mycroft skills that allows the user to control the Mycroft Plasmoid via Voice
+## plasma-mycroftplasmoid-control
+This skill allows control of the Mycroft Plasmoid itself
+
+## Description 
+#### Installation of skill:
+msm install plasma-mycroftplasmoid-control
+
+### Manual Method
+* Download or Clone Git
+* Create /opt/mycroft/skills folder if it does not exist
+* Extract Downloaded Skill into a folder. "plasma-mycroftplasmoid-control". (Clone does not require this step)
+* Copy the plasma-mycroftplasmoid-control folder to /opt/mycroft/skills/ folder
+
+##### Python Dbus is required for this skill
+
+## Examples 
+* "Hey Mycroft, show mycroft applet"
+* "Hey Mycroft, show skills"
+* "Hey Mycroft, display skills page"
+
+## Credits 
+Aix
+
+## Require 
+platform_plasmoid 
+
+## Exclude 
+platform_mark1 platform_picroft 
---
+++ __init__.py
@@ -0,0 +1,51 @@
+import sys
+import dbus
+from traceback import print_exc
+from os.path import dirname
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+__author__ = 'aix'
+
+LOGGER = getLogger(__name__)
+
+
+class MycroftPlasmoidControlSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(MycroftPlasmoidControlSkill, self).__init__(name="MycroftPlasmoidControlSkill")
+
+    # This method loads the files needed for the skill's functioning, and
+    # creates and registers each intent that the skill uses
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+
+        mycroftplasmoid_plasma_show_intent = IntentBuilder("MycroftPlasmoidShowKeywordIntent").\
+            require("MycroftPlasmoidShowKeyword").build()
+        self.register_intent(mycroftplasmoid_plasma_show_intent, self.handle_mycroftplasmoid_plasma_show_intent)
+
+        mycroftplasmoid_plasma_showskills_intent = IntentBuilder("MycroftPlasmoidShowSkillsKeywordIntent").\
+            require("MycroftPlasmoidShowSkillsKeyword").build()
+        self.register_intent(mycroftplasmoid_plasma_showskills_intent, self.handle_mycroftplasmoid_plasma_showskills_intent)
+
+    def handle_mycroftplasmoid_plasma_show_intent(self, message):
+
+        bus = dbus.SessionBus()
+        remote_object = bus.get_object("org.kde.mycroftapplet", "/mycroftapplet")
+        remote_object.showMycroft(dbus_interface="org.kde.mycroftapplet")
+
+    def handle_mycroftplasmoid_plasma_showskills_intent(self, message):
+
+        bus = dbus.SessionBus()
+        remote_object = bus.get_object("org.kde.mycroftapplet", "/mycroftapplet")
+        remote_object.showMycroft(dbus_interface="org.kde.mycroftapplet")
+
+    def stop(self):
+        pass
+
+# The "create_skill()" method is used to create an instance of the skill.
+# Note that it's outside the class itself.
+    def create_skill():
+        return MycroftPlasmoidControlSkill()
